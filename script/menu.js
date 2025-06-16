5e962399df66aad8a3606ed39765154ecfb6c96a
JaenyungJeong <rantelet@gmail.com>
2025-06-16 20:33:38 +0900

    20:33

--- script/menu.js
+++ script/menu.js
@@ -42,3 +42,47 @@ $(document).ready(function () {
         }); 
     });
 });
+
+
+$(document).ready(function () {
+
+    //초기설정 - 슬라이드 배치 설정
+    //이전 버튼 클릭을 대비하여 마지막 li를 첫번째로 이동
+
+    $('.handmade-box_wrap .handmade-box:last').prependTo('.handmade-box_wrap');
+
+    //li 한 칸의 크기 구하기 = 슬라이드 하나의 크기 구하기!
+    var liW = $('.handmade-box_wrap .handmade-box').outerWidth();
+    console.log('li하나의 너비:' + liW);
+
+    $('.handmade-box_wrap').css('margin-left', -liW);
+
+    //다음 버튼을 클릭했을 때!
+    $('.next2 a').click(function (e) {
+
+        e.preventDefault();
+        $('.handmade-box_wrap').animate({
+            marginLeft: '-=' + liW //클릭했을 때 마진 -1800이 되어야함 
+        }, 800, function () {
+            //다음(두번째 클릭)을 위한 준비!
+            //1)첫번째 li(슬라이드)를 맨 뒤로 보내기 >> append()
+            //2)슬라이드 순서 변경에 따른 ul의 위치 조절하기 >> margin
+            $('.handmade-box_wrap .handmade-box').first().appendTo('.handmade-box_wrap')
+            $('.handmade-box_wrap').css('margin-left', -liW);
+        });
+
+    });
+
+    //이전 버튼을 클릭했을 때
+    $('.prev2 a').click(function(e){
+        e.preventDefault();
+        $('.handmade-box_wrap').animate({
+            marginLeft : '+=' + liW //클릭했을 때 마진 0이 되어야함 
+        },800, function(){
+            //다음을 위한 준비!!
+            $('.handmade-box_wrap .handmade-box:last').prependTo('.handmade-box_wrap');
+            $('.handmade-box_wrap').css('margin-left', -liW);
+        }); 
+    });
+});
+
