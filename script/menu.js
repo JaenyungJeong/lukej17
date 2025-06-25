1a8e4287845f6ef9c7fa3a35550fdee6602f9536
JaenyungJeong <rantelet@gmail.com>
2025-06-25 16:28:43 +0900

    16:28

--- script/menu.js
+++ script/menu.js
@@ -63,6 +63,9 @@ $(document).ready(function () {
         e.preventDefault();
 
         //맨 앞에 있는 박스를 맨 뒤로 보낸 뒤에 이동! 
+
+        /* $('.handmade-box_wrap .handmade-box:first').appendTo('.handmade-box_wrap');
+        $('.handmade-box_wrap').css('margin-left', -liW);  */
         
         $('.handmade-box_wrap').animate({
             marginLeft: '-=' + liW //클릭했을 때 마진 -1800이 되어야함 
@@ -85,6 +88,54 @@ $(document).ready(function () {
             //다음을 위한 준비!!
             $('.handmade-box_wrap .handmade-box:last').prependTo('.handmade-box_wrap');
             $('.handmade-box_wrap').css('margin-left', -liW);
+        }); 
+    });
+});
+
+$(document).ready(function () {
+
+    //초기설정 - 슬라이드 배치 설정
+    //이전 버튼 클릭을 대비하여 마지막 li를 첫번째로 이동
+
+    $('.brewery-box_wrap .breweru-box:last').prependTo('.brewery-box_wrap');
+
+    //li 한 칸의 크기 구하기 = 슬라이드 하나의 크기 구하기!
+    var liW = $('.brewery-box_wrap .brewery-box').outerWidth(true);
+    console.log('li하나의 너비:' + liW);
+
+    $('.brewery-box_wrap').css('margin-left', -liW);
+
+    //다음 버튼을 클릭했을 때!
+    $('.next2 a').click(function (e) {
+
+        e.preventDefault();
+
+        //맨 앞에 있는 박스를 맨 뒤로 보낸 뒤에 이동! 
+
+        /* $('.handmade-box_wrap .handmade-box:first').appendTo('.handmade-box_wrap');
+        $('.handmade-box_wrap').css('margin-left', -liW);  */
+        
+        $('.brewery-box_wrap').animate({
+            marginLeft: '-=' + liW //클릭했을 때 마진 -1800이 되어야함 
+        }, 800, function () {
+            //다음(두번째 클릭)을 위한 준비!
+            //1)첫번째 li(슬라이드)를 맨 뒤로 보내기 >> append()
+            //2)슬라이드 순서 변경에 따른 ul의 위치 조절하기 >> margin
+            $('.brewery-box_wrap .brewery-box').first().appendTo('.brewery-box_wrap')
+            $('.brewery-box_wrap').css('margin-left', -liW);
+        });
+
+    });
+
+    //이전 버튼을 클릭했을 때
+    $('.prev2 a').click(function(e){
+        e.preventDefault();
+        $('.brewery-box_wrap').animate({
+            marginLeft : '+=' + liW //클릭했을 때 마진 0이 되어야함 
+        },800, function(){
+            //다음을 위한 준비!!
+            $('.brewery-box_wrap .brewery-box:last').prependTo('.brewery-box_wrap');
+            $('.brewery-box_wrap').css('margin-left', -liW);
         }); 
     });
 });
--- script/space.js
+++ script/space.js
@@ -1,18 +1,7 @@
-//space.js 
+// banapresso250617.js
 
-// $(document).ready(function(){
 
-//     var box = $('.box1:eq(0) , .box1:eq(1)');  
-//     // 선택자를 변수에 담는 이유: 반복되거나 여러군데에서 쓰일때
-
-//     box.find('.img-box1').animate({
-//         width : '50%' 
-//     }, 800, function(){
-//         box.find('.txt-box1').fadeIn(600)
-//         $('.txt-box2-1').fadeIn(600)
-//     });
-// });
-
+// 스크롤이 될 수 있는 범위는 스크롤이 넘치는 안보이는 영역만큼
 
 $(window).scroll(function(){
 
@@ -25,63 +14,57 @@ $(window).scroll(function(){
     var gap = Math.ceil(winHeight * 0.9);
     console.log('gap: ' + gap);
 
+    /* 
+    수학객체 메서드를 통해 정수 반환받기
 
-    //1번 박스 등장
+    Math.ceil(값); - 소숫점 첫째자리에서 '무조건' 올림
+    Math.floor(값); - 소숫점 첫째자리에서 '무조건' 내림
+    Math.round(값); - 소숫점 첫째자리에서 반올림
 
-    var box1pos = $('.box1').eq(0).offset().top - gap; 
-    console.log('3번째 박스 등장 스크롤 기준값: ' + box3pos);
-
-    if (scTop > box1pos) {
-        $('.box1').eq(0).find('.img-box1').animate({
-            width: '50%'
-        },800 , function(){
-            $(this).next().fadeIn(600);
-            $('.txt-box1').fadeIn(600)
-            $('.txt-box2-1').fadeIn(600)
-        });
-    }
-
-    //2번 박스 등장
-
-    var box2pos = $('.box2').eq(0).offset().top - gap; 
-    console.log('3번째 박스 등장 스크롤 기준값: ' + box3pos);
-
-    if (scTop > box2pos) {
-        $('.box2').eq(0).find('.img-box2').animate({
-            width: '50%'
-        },800 , function(){
-            $(this).next().fadeIn(600);
-            $('.txt-box2').fadeIn(600)
-            $('.txt-box2-2').fadeIn(600)
-        });
-    }
+    
+    */
 
     //3번 박스 등장
 
-    var box3pos = $('.box3').eq(0).offset().top - gap; 
+    var box1pos = $('.box').eq(0).offset().top - gap; 
     console.log('3번째 박스 등장 스크롤 기준값: ' + box3pos);
 
-    if (scTop > box3pos) {
-        $('.box3').eq(0).find('.img-box3').animate({
+    if (scTop > box1pos) {
+        $('.box').eq(0).find('.img-box').animate({
             width: '50%'
         },800 , function(){
             $(this).next().fadeIn(600);
-            $('.txt-box3').fadeIn(600)
-            $('.txt-box2-3').fadeIn(600)
         });
     }
 
     //4번 박스 등장
-    var box4pos = $('.box4').eq(0).offset().top - gap;
+    var box2pos = $('.box').eq(1).offset().top - gap;
 
-    if(scTop > box4pos) {
-        $('.box4').eq(0).find('.img-box4').animate({
+    if(scTop > box2pos) {
+        $('.box').eq(1).find('.img-box').animate({
             width: '50%'
         },800, function(){
             $(this).prev().fadeIn(600);
-            $('.txt-box4').fadeIn(600)
-            $('.txt-box2-4').fadeIn(600)
         });
     }
 
+    var box3pos = $('.box').eq(2).offset().top - gap;
+
+    if(scTop > box3pos) {
+        $('.box').eq(2).find('.img-box').animate({
+            width: '50%'
+        },800, function(){
+            $(this).next().fadeIn(600);
+        });
+    }
+
+    var box4pos = $('.box').eq(3).offset().top - gap;
+
+    if(scTop > box4pos) {
+        $('.box').eq(3).find('.img-box').animate({
+            width: '50%'
+        },800, function(){
+            $(this).prev().fadeIn(600);
+        });
+    }
 });
